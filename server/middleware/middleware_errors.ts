21d3f9d4c664b3f7ff4aa5a31760453ca7fd4159
Jacob Rush <rushmjake@gmail.com>
2026-06-22 13:42:55 -0700

    Middleware errors now handles the response for thrown errors

--- server/middleware/middleware_errors.ts
+++ server/middleware/middleware_errors.ts
@@ -16,32 +16,17 @@ export const middlewareErrors = (
 ) => {
   if (err instanceof NotFoundError) {
     return res.status(404).json({ success: false, error: err.message });
-  }
-  if (err instanceof UnauthorizedError || err instanceof InvalidTokenError) {
-    res.cookie("refreshToken", "", {
-      sameSite: "strict",
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      maxAge: 0,
-      path: "/api/auth/refresh",
-    });
-    res.cookie("token", "", {
-      sameSite: "strict",
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      maxAge: 0,
-    });
+  } else if (err instanceof UnauthorizedError) {
     return res
       .status(401)
       .json({ success: false, error: "Unauthorized, please login" });
-  }
-  if (err instanceof BadRequestError) {
+  } else if (err instanceof BadRequestError) {
     return res.status(400).json({ success: false, error: err.message });
-  }
-  if (err instanceof ForbiddenError) {
+  } else if (err instanceof ForbiddenError) {
     return res.status(403).json({ success: false, error: err.message });
-  }
-  if (err instanceof InternalServerError) {
+  } else if (err instanceof InternalServerError) {
+    return res.status(500).json({ success: false, error: err.message });
+  } else {
     return res.status(500).json({ success: false, error: err.message });
   }
 };
